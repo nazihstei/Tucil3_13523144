b62822b14bf0e1163a7d06cb8b8a8a6a0d0c1bd4
Nazih Najmudin <163629526+nazihstei@users.noreply.github.com>
2025-05-17 22:14:07 +0700

    feat: depth of BoardTree

--- src/model/BoardTree.java
+++ src/model/BoardTree.java
@@ -1,20 +1,34 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class BoardTree {
     
     /* ATTRIBUTE */
+    private BoardTree root;
     private Board node;
-    private ArrayList<Board> branches;
+    private ArrayList<BoardTree> branches;
+    private long depth;
 
     /* CONSTRUCTOR */
     public BoardTree(Board node) {
+        this.root = null;
         this.node = node;
+        this.depth = 0;
+        this.branches = new ArrayList<>();
+    }
+    public BoardTree(Board node, BoardTree root) {
+        this.root = root;
+        this.node = node;
+        this.depth = root.getDepth() + 1;
         this.branches = new ArrayList<>();
     }
 
     /* GETTER and SETTER */
+    public long getDepth() {
+        return this.depth;
+    }
     public Board getNode() {
         return this.node;
     }
@@ -25,14 +39,19 @@ public class BoardTree {
     /* BRANCHES */
     public void addBranch(Board b) {
         if (b == null) return;
-        this.branches.add(b);
+        this.branches.add(new BoardTree(b, this));
     }
-    public void addBranch(ArrayList<Board> data) {
+    public void addBranch(Collection<Board> data) {
         if (data == null) return;
-        this.branches = data;
+        for (Board b : data) {
+            this.addBranch(b);
+        }
     }
 
     /* CONDITIONAL CHECK */
+    public Boolean isRoot() {
+        return this.root == null;
+    }
     public Boolean isLeaf() {
         return this.branches.size() == 0;
     }
